7b76e44fc4fa89187538ef4877ebf101c8393308
Brayden Fleener <89265360+Brayden-Fleener@users.noreply.github.com>
2021-10-25 16:29:52 +0000

    Added an object & added collision to it

--- script.js
+++ script.js
@@ -6,16 +6,24 @@ const game = new Phaser.Game(800, 800, Phaser.AUTO, '', {
 
 let cursors
 let player
+let object
 
 function preload () {
   game.load.image('sky', 'assets/backgrounds/Pokemon.png')
   game.load.spritesheet('boy', 'assets/sprites/boy_run_left_right.png', 31.99, 43)
+  game.load.image('object','assets/sprites/black1.png')
 }
 
 function create () {
   game.physics.startSystem(Phaser.Physics.ARCADE)
 
   game.add.sprite(0, 0, 'sky')
+
+  object = game.add.group()
+  object.enableBody = true
+  const object1 = object.create(0, game.world.height - 500, 'object')
+  //object1.scale.setTo(1, 1)
+  object1.body.immovable = true
 
   player = game.add.sprite(370, game.world.height -100, 'boy')
 
@@ -32,7 +40,7 @@ function create () {
 }
 
 function update () {
-  game.physics.arcade.collide(player)
+  game.physics.arcade.collide(player, object)
 
   game.physics.arcade.overlap(player, null, this)
 
